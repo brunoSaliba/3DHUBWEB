c464cf0cd23615276cebbc93efa8b845d0a8e2af
brunoSaliba <48458294+brunoSaliba@users.noreply.github.com>
2019-05-23 19:52:20 -0300

    Add files via upload

--- telas/login.php
+++ telas/login.php
@@ -1,5 +1,5 @@
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<link rel="stylesheet" type="text/css" href="css/login.css">
+<link rel="stylesheet" type="text/css" href="../css/login.css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
@@ -13,7 +13,7 @@
 		<input type="text" class="form-control" placeholder="Usuario" autofocus="">
 		<input type="password" class="form-control" placeholder="Senha" >
 		<center><button class="btn btn-lg btn-success" type="submit">
-		<a href="telas/pesquisa.php">	
+		<a href="pesquisa.php">	
 		Entrar
 		</a></button></center>
 	</form>
