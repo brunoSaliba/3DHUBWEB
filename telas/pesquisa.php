6540b166a73b59db05c7d6a15c64991501130429
brunoSaliba <48458294+brunoSaliba@users.noreply.github.com>
2019-05-23 19:41:19 -0300

    Upload Inicial

---
+++ telas/pesquisa.php
@@ -0,0 +1,44 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" type="text/css" href="../css/pesquisa.css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
+
+<center><div class="container"></center>
+    <br/>
+    <div id="fullscreen_bg" class="fullscreen_bg"/>
+	<div class="row justify-content-center">    
+                        <div class="col-12 col-md-10 col-lg-8">
+                            <form class="card card-sm" action="imprimindo.php" method="post" enctype="multipart/form-data">
+                                <div class="card-body row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <i class="fas fa-search h4 text-body"></i>
+                                    </div>
+                                    <!--end of col-->
+                                    <div class="col">
+                                        <input class="form-control form-control-lg form-control-borderless" type="file" name="file" id="file">
+                                    </div>
+                                    <!--end of col-->
+                                    <div class="col-auto">
+                                        <button class="btn btn-lg btn-success" type="submit">
+                                        <a href="imprimindo.php">	
+                                        Imprimir
+                                        </a></button>
+                                    </div>                                
+                                    <!--end of col-->
+                                </div>
+                            </form>
+                            <div class="col-auto">
+                                        <center><button class="btn btn-lg btn-success" type="submit">
+                                        <a href="projetos.php">	
+                                        Projetos no Banco
+                                        </a></button></center>
+                            </div> 
+                        </div>
+                        <!--end of col-->
+                    </div>
+</div>
---
+++ telas/projetos.php
@@ -0,0 +1,144 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" type="text/css" href="../css/projetos.css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons">
+</head>
+
+
+<body>
+    
+   
+    <div class="container mt-5">
+        <div class="title">
+          <h3>Projetos do Banco</h3>
+        </div>
+        <center><div class="col-auto">
+                <button class="btn btn-lg btn-success" type="submit">
+                <a href="pesquisa.php">	
+                Voltar
+                </a></button>
+        </div> </center>
+        
+        <div class="row">
+            <div class="col-sm-2 ml-auto">
+                <h4>Ender 5</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/1s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Tool T1</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/2s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Gear T1</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/3s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Gear T2</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/4s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Tool Support</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/5s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Surface Pro</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/6s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Tape Roll</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/7s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Utility Handle</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/8s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Prusa</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/16s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Utility Handle</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/10s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Screws</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/11s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Sanding Tool</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/12s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Support tool</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/13s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+            <div class="col-sm-2 ml-auto">
+                <h4>Sanding Tool</h4>
+                <a href="imprimindo.php">
+                <img src="../imagens/14s.jpg" alt="Circle Image" class="rounded-circle img-fluid">
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+    <div >
+
+    
+    <footer class="footer text-center ">
+        <p>Projetos 3D HUB</p>
+    </footer>
+
+    
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+
+</body>
